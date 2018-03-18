397663070d1cef4b0b80534e328335633a625e94
gravestoned <odillner@gmail.com>
2018-03-18 14:05:04 +0100

    initial

---
+++ cmake-build-debug/CMakeFiles/Particle-Sim-Pthreads.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/d/dev/particle-sim-pthreads-master/src/common.cpp" "/cygdrive/d/dev/particle-sim-pthreads-master/cmake-build-debug/CMakeFiles/Particle-Sim-Pthreads.dir/src/common.cpp.o"
+  "/cygdrive/d/dev/particle-sim-pthreads-master/src/main.cpp" "/cygdrive/d/dev/particle-sim-pthreads-master/cmake-build-debug/CMakeFiles/Particle-Sim-Pthreads.dir/src/main.cpp.o"
+  "/cygdrive/d/dev/particle-sim-pthreads-master/src/pthread.cpp" "/cygdrive/d/dev/particle-sim-pthreads-master/cmake-build-debug/CMakeFiles/Particle-Sim-Pthreads.dir/src/pthread.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Particle-Sim-Pthreads.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Particle-Sim-Pthreads.dir/src/common.cpp.o"
+  "CMakeFiles/Particle-Sim-Pthreads.dir/src/main.cpp.o"
+  "CMakeFiles/Particle-Sim-Pthreads.dir/src/pthread.cpp.o"
+  "Particle-Sim-Pthreads.pdb"
+  "Particle-Sim-Pthreads.exe"
+  "libParticle-Sim-Pthreads.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Particle-Sim-Pthreads.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
